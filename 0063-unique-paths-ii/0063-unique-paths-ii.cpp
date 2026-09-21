fc4c40bbff9e25e5fa077642faaff0da64389eeb
Tanisha6462 <lambatanisha021@gmail.com>
2026-09-21 19:27:07 +0530

    0063-unique-paths-ii Array

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int t[1001][1001];
+    int m,n;
+    int solve(int i, int j , vector<vector<int>>& obstacleGrid){
+        if(i < 0 || i >= m || j < 0 || j >= n || obstacleGrid[i][j] == 1){
+            return 0;
+        }
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+        int right = solve(i,j+1,obstacleGrid);
+        int bottom = solve(i+1,j,obstacleGrid);
+
+        return t[i][j] = right + bottom;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        memset(t,-1,sizeof(t));
+        m = obstacleGrid.size();
+        n = obstacleGrid[0].size();
+        return solve(0,0,obstacleGrid);
+        
+    }
+};
